47607562d9bc65796f5df88a7815af74cecd7a58
dqvuong2111 <dqvuong2111@gmail.com>
2025-11-22 16:18:27 +0700

    Update context

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,13 +1,30 @@
 import { createContext } from "react";
 import { food_list } from "../assets/assets";
+import { useState } from "react";
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
+    const [cartItems, setCartItems] = useState({});
 
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        } else {
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
 
     return (
